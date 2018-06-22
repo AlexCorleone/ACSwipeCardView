6a3fec6330139491439146403b135d1974acab51
AlexCorleone <weiqiankun@268xue.com>
2018-06-22 10:32:35 +0800

    Initial Commit

---
+++ ACSwipeCardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ACSwipeCardView
+//
+//  Created by AlexCorleone on 2018/6/22.
+//  Copyright © 2018年 AlexCorleone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ACSwipeCardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ACSwipeCardView
+//
+//  Created by AlexCorleone on 2018/6/22.
+//  Copyright © 2018年 AlexCorleone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
